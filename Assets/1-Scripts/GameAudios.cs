b0fe3a0b2604639b29f028c29fdea69dcdeb2a5f
Butter <zhanShun5@users.noreply.github.com>
2026-03-26 11:46:03 +0100

    audioSFX

--- Assets/1-Scripts/GameAudios.cs
+++ Assets/1-Scripts/GameAudios.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameAudios", menuName = "ScriptableObjects/GameAudios", order = 1)]
 public class GameAudios : ScriptableObject
 {
-    public AudioClip matchAudio;
+    /// Audio
+    public AudioClip matchAudio; 
+    public AudioClip goodMatchAudio;
+    public AudioClip badMatchAudio;
+    public AudioClip badMatchAudio2;
+    public AudioClip nextTargetAudio;
+    public AudioClip buttonClickAudio;
+    public AudioClip endOfDayAudio;
+    public AudioClip mouseClickAudio;
+    public AudioClip startGameClickAudio;
+    public AudioClip startGameClickAudio2;
 }
